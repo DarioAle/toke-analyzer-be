28b251352f60ef44eb2b9f3dc0b9606276afa133
darioala <darioala@localhost.localdomain>
2021-03-19 20:03:45 -0600

    adding api to my own function

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,36 +5,16 @@ const ToneAnalyzerV3 = require('ibm-watson/tone-analyzer/v3');
 const { IamAuthenticator } = require('ibm-watson/auth');
 
 const apikey = "IK1qS8GKd61THsn7Ea4MELhjYPVpcTkCc4m03w0Zt_2f";
-const version = "2017-09-21"; 
-const url = "https://api.us-south.tone-analyzer.watson.cloud.ibm.com/instances/875eabee-ae6e-4f87-905e-5664456f0264/v3/tone";
+const url = 'https://api.us-south.tone-analyzer.watson.cloud.ibm.com/instances/875eabee-ae6e-4f87-905e-5664456f0264/v3/tone?version=2017-09-21';
 
 
 const toneAnalyzer = new ToneAnalyzerV3({
-    version: version,
+    version: '2017-09-21',
     authenticator: new IamAuthenticator({
-      apikey: apikey,
+      apikey: `${apikey}`
     }),
-    serviceUrl: url,
+    serviceUrl: `${url}`,
   });  
-
-  const text = 'Team, I know that times are tough! Product '
-  + 'sales have been disappointing for the past three '
-  + 'quarters. We have a competitive product, but we '
-  + 'need to do a better job of selling it!';
-
-  const toneParams = {
-    toneInput: { 'text': text },
-    contentType: 'application/json',
-    sentences : false
-  };
-  
-  toneAnalyzer.tone(toneParams)
-    .then(toneAnalysis => {
-      console.log(JSON.stringify(toneAnalysis, null, 2));
-    })
-    .catch(err => {
-      console.log('error:', err);
-    });
 
 // importamos los routes
 const homeRoute = require('./home');
@@ -65,7 +45,24 @@ router.route('/autor').get((req,res) =>{
     )
 })
 
-router.route('/tone').get((req,res) =>{
+router.route('/tone').post((req,res) =>{
+    const text = req.body;
+
+    console.log(text.text);
+
+    const toneParams = {
+        toneInput: { 'text': text },
+        contentType: 'application/json',
+        sentences : false
+      };
+      
+    // toneAnalyzer.tone(toneParams)
+    //             .then(toneAnalysis => {
+    //                     console.log(JSON.stringify(toneAnalysis, null, 2));
+    //                 })
+    //             .catch(err => {
+    //                     console.log('error:', err);
+    //             });
     res.json(
         {
             instructions : "TODO Make a post to this url",
