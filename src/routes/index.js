b33f3eefbb0bc9cf589dff04452a5d759e9b3e47
darioala <darioala@localhost.localdomain>
2021-03-19 19:35:14 -0600

    testing the api in the code

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,41 @@
 const router = require('express').Router();
 
+// Tone analyzer libraries
+const ToneAnalyzerV3 = require('ibm-watson/tone-analyzer/v3');
+const { IamAuthenticator } = require('ibm-watson/auth');
+
+const apikey = "IK1qS8GKd61THsn7Ea4MELhjYPVpcTkCc4m03w0Zt_2f";
+const version = "2017-09-21"; 
+const url = "https://api.us-south.tone-analyzer.watson.cloud.ibm.com/instances/875eabee-ae6e-4f87-905e-5664456f0264/v3/tone";
+
+
+const toneAnalyzer = new ToneAnalyzerV3({
+    version: version,
+    authenticator: new IamAuthenticator({
+      apikey: apikey,
+    }),
+    serviceUrl: url,
+  });  
+
+  const text = 'Team, I know that times are tough! Product '
+  + 'sales have been disappointing for the past three '
+  + 'quarters. We have a competitive product, but we '
+  + 'need to do a better job of selling it!';
+
+  const toneParams = {
+    toneInput: { 'text': text },
+    contentType: 'application/json',
+    sentences : false
+  };
+  
+  toneAnalyzer.tone(toneParams)
+    .then(toneAnalysis => {
+      console.log(JSON.stringify(toneAnalysis, null, 2));
+    })
+    .catch(err => {
+      console.log('error:', err);
+    });
+
 // importamos los routes
 const homeRoute = require('./home');
 
